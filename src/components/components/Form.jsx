8dc7de5ef486efeac792ff017cc58f2e04b7cad5
iairbaron <iairbaron@gmail.com>
2023-08-09 18:30:51 -0300

    Formulario parte ui terminado, falta funcionalidad

--- src/components/components/Form.jsx
+++ src/components/components/Form.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
-import Input from '@mui/material/Input';
-
-
-
-const ariaLabel = { 'aria-label': 'description' };
+import axios from 'axios';
 
 const theme = createTheme({
     palette: {
@@ -18,32 +14,66 @@ const theme = createTheme({
 const Form = () => {
     const [nombre, setNombre] = useState('');
     const [descripcion, setDescripcion] = useState('');
-    const [url, seturl] = useState('');
-    const [msj, setmsj] = useState('');
+    const [urls, setUrls] = useState([]);
+    const [msj, setMsj] = useState('');
     const [cantUrl, setCantUrl] = useState('');
 
-    
-
-
-
     function handleSubmit(e) {
-        e.preventDefault()
-
+        e.preventDefault();
         const producto = {
             Pnombre: nombre.trim(),
             Pdescripcion: descripcion,
-            Purl: url
-        }
+            Purls: urls
+        };
 
+        // Realizar la solicitud POST utilizando Axios
+        axios.post('URL_DE_TU_API', producto)
+            .then(response => {
+                console.log('Producto enviado:', response.data);
+                // Limpiar el formulario
+                setNombre('');
+                setDescripcion('');
+                setUrls([]);
+                setCantUrl('');
+                setMsj('Producto enviado exitosamente');
+            })
+            .catch(error => {
+                console.error('Error al enviar el producto:', error);
+                setMsj('Hubo un error al enviar el producto.');
+            });
     }
+
+    function handleUrlChange(index, value) {
+        const newUrls = [...urls];
+        newUrls[index] = value;
+        setUrls(newUrls);
+    }
+
+    function renderUrlInputs() {
+        return Array.from({ length: Number(cantUrl) }, (_, index) => (
+            <TextField
+                key={index}
+                label={`URL ${index + 1}`}
+                variant="outlined"
+                fullWidth
+                size="small"
+                value={urls[index] || ''}
+                onChange={(e) => handleUrlChange(index, e.target.value)}
+                style={{ marginBottom: '2px' }} // Agrega un estilo para reducir la altura
+
+            />
+        ));
+    }
+
     return (
         <ThemeProvider theme={theme}>
             <div>
-                <form action="" onSubmit={handleSubmit} className='flex flex-col gap-6 w-80'>
+                <form onSubmit={handleSubmit} className='flex flex-col gap-6 w-80'>
                     <TextField
                         label="Nombre"
                         variant="outlined"
                         fullWidth
+                        value={nombre}
                         onChange={(e) => setNombre(e.target.value)}
                     />
 
@@ -53,38 +83,33 @@ const Form = () => {
                         multiline
                         rows={4}
                         fullWidth
+                        value={descripcion}
                         onChange={(e) => setDescripcion(e.target.value)}
                     />
 
                     <select
                         className='border rounded px-2 py-1'
+                        value={cantUrl}
                         onChange={(e) => setCantUrl(e.target.value)}
                     >
-                        <option value=''>Selecciona una opción</option>
-                        <option value={1}> 1</option>
-                        <option value={2}> 2</option>
-                        <option value={3}> 3</option>
-                        <option value={4}> 4</option>
-                        
+                        <option value=''>Selecciona cantidad de urls</option>
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
                     </select>
 
-                                        
-                    {/* <Input defaultValue="Hello world" inputProps={ariaLabel} /> */}
+                    {renderUrlInputs()}
 
-                    
-
-                    
-
-                    <p className=' text-[red]'>{msj}</p>
+                    <p className='text-[red]'>{msj}</p>
 
                     <button className='btn h-10 bg-gradient-to-r from-[#9EDE82] to-[#9EDE80] text-white font-semibold px-3 py-1 rounded duration-500 md:static' type='submit'>Enviar</button>
-                    {url && <div className='text-center'>{url}</div>}
 
                 </form>
-
             </div>
         </ThemeProvider>
-    )
+    );
 }
 
-export default Form
+export default Form;
